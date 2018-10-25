c8944a19e004997dfeafcc76d1bdd54947410ae0
Leo Zhou <glasslion@gmail.com>
2018-10-25 16:44:09 +0800

    add tests

--- tests/test_ticktick.py
+++ tests/test_ticktick.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """Tests for `ticktick` package."""
 
+import os
+from datetime import datetime
 import pytest
 
-
-from ticktick import ticktick
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from ticktick import TickTick
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_login():
+    tt = TickTick(
+        username=os.environ['TICKTICK_USERNAME'],
+        password=os.environ['TICKTICK_PASSWORD'],
+        auto_logion=False,
+    )
+    r = tt._login()
+    assert r.status_code == 200
+
+
+def test_fetch():
+    tt = TickTick(
+        username=os.environ['TICKTICK_USERNAME'],
+        password=os.environ['TICKTICK_PASSWORD'],
+    )
+    tt.fetch(datetime(2018, 10, 1), datetime(2018, 10, 31), 50)
+    assert len(tt.tasks) > 0
+    assert len(tt.tasks) == len(tt.completed) +  len(tt.uncompleted)
