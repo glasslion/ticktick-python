333b259270b1dc371596790a6f1a497ebc4ae3f6
Leo Zhou <glasslion@gmail.com>
2018-10-25 18:34:25 +0800

    add requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['requests', 'addict', 'six', 'bson']
+requirements = ['requests', 'addict', 'arrow', 'six', 'bson']
 
 setup_requirements = ['pytest-runner', ]
 
